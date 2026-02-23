2cdd5ff06a834fdb6d546ac36b334fb034f4743a
wordik <verduk.nikita07@gmail.com>
2026-02-23 23:45:51 +0700

    Создал функцию createUser

--- main.ts
+++ main.ts
@@ -4,3 +4,12 @@ interface User{
     email?: string;
     isActive: boolean;
 }
+
+function createUser(id: number, name: string, email?: string, isActive: boolean = true) : User{
+    return {
+        id,
+        name,
+        email,
+        isActive
+    };
+}
