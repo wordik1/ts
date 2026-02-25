31d212d3defc03fe9aa30aa7f3af5d883f63bfd4
wordik <verduk.nikita07@gmail.com>
2026-02-25 20:43:22 +0700

    Сделал тест createUser

--- src/main.test.ts
+++ src/main.test.ts
@@ -2,7 +2,18 @@ import { it, describe, expect } from 'vitest';
 import { createUser, createBook, calculateArea, getStatusColor, upperFirst, trimSpaces, getFirstElement, findById } from './main.ts';
 
 describe('test of functions', () =>{
-    
+
+    it('createUser test',
+        () => {
+            const result = createUser(1, "Nick", "bimbimbambam@gmail.com");
+            expect(result).toBeTypeOf('object');
+            expect(result.id).toBe(1);
+            expect(result.name).toBe("Nick");
+            expect(result.email).toBe("bimbimbambam@gmail.com");
+            expect(result.isActive).toBe(true);
+        }
+    )
+
     it('circle calculateArea',
         () => {
             let result = calculateArea('circle', 3);
