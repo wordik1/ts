ce1157d0af6dc388bd490501543ff91583cb5bd5
wordik <verduk.nikita07@gmail.com>
2026-03-07 20:03:22 +0700

    Реализовал тип Group

--- src/4lab/4lab.ts
+++ src/4lab/4lab.ts
@@ -1,3 +1,4 @@
 type Transform<T> = (data: T[]) => T;
 type Where<T, K extends keyof T> = (key: K, value: T[K]) => Transform<T>;
 type Sort<T> = <K extends keyof T>(key: k) => Transform<T>;
+type Group<T, K extends keyof T> = { key: T[K], items: T[] }; 
