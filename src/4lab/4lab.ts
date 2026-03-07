309be181dca4c9d772dbb331a3c260f61c396a30
wordik <verduk.nikita07@gmail.com>
2026-03-07 22:26:53 +0700

    Реализовал тип Having

--- src/4lab/4lab.ts
+++ src/4lab/4lab.ts
@@ -4,3 +4,4 @@ type Sort<T> = <K extends keyof T>(key: K) => Transform<T>;
 type Group<T, K extends keyof T> = { key: T[K], items: T[] };
 type GroupBy<T> = <K extends keyof T>(key: K) => Transform<Group<T, K>>;
 type GroupTransform<T, K extends keyof T> = (groups: Group<T, K>[]) => Group<T, K>[];
+type Having<T> = <T, K extends keyof T>(predicate: (group: Group<T, K>) => boolean) => GroupTransform<T, K>;
