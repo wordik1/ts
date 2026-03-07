265855ad0fc87a79d7de9f7d9dd839b5aa042004
wordik <verduk.nikita07@gmail.com>
2026-03-07 23:39:38 +0700

    Реализовал тип Step

--- src/4lab/4lab.ts
+++ src/4lab/4lab.ts
@@ -5,3 +5,5 @@ type Group<T, K extends keyof T> = { key: T[K], items: T[] };
 type GroupBy<T> = <K extends keyof T>(key: K) => Transform<Group<T, K>>;
 type GroupTransform<T, K extends keyof T> = (groups: Group<T, K>[]) => Group<T, K>[];
 type Having<T> = <T, K extends keyof T>(predicate: (group: Group<T, K>) => boolean) => GroupTransform<T, K>;
+
+type Step<T> = Transform<T> | GroupTransform<T, any>;
