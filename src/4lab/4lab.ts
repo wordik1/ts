8303c51a944e0d516df7c3041f12bf5502db444d
wordik <verduk.nikita07@gmail.com>
2026-03-07 23:46:38 +0700

    Реализовал функцию query

--- src/4lab/4lab.ts
+++ src/4lab/4lab.ts
@@ -1,4 +1,4 @@
-type Transform<T> = (data: T[]) => T;
+type Transform<T> = (data: T[]) => T[];
 type Where<T, K extends keyof T> = (key: K, value: T[K]) => Transform<T>;
 type Sort<T> = <K extends keyof T>(key: K) => Transform<T>;
 type Group<T, K extends keyof T> = { key: T[K], items: T[] };
@@ -7,3 +7,14 @@ type GroupTransform<T, K extends keyof T> = (groups: Group<T, K>[]) => Group<T, 
 type Having<T> = <T, K extends keyof T>(predicate: (group: Group<T, K>) => boolean) => GroupTransform<T, K>;
 
 type Step<T> = Transform<T> | GroupTransform<T, any>;
+function query<T>(...steps: Step<T>[]): Transform<T>{
+    return(data: T[]) => {
+        let result: any = data;
+
+        for (const step of steps){
+            result = step(result);
+        }
+
+        return result as T[];
+    };
+}
