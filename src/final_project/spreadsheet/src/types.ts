b9579baaf1f5b75d7e39c4329306cefe469bd501
wordik <verduk.nikita07@gmail.com>
2026-05-09 15:29:29 +0700

    Добавил типы

---
+++ src/final_project/spreadsheet/src/types.ts
@@ -0,0 +1,45 @@
+export type CellFormat = 'default' | 'percent' | 'currency' | 'date';
+export type CellAlign = 'left' | 'center' | 'right';
+
+export interface CellStyle {
+  bold: boolean;
+  italic: boolean;
+  underline: boolean;
+  bg: string;
+  color: string;
+  align: CellAlign;
+  format: CellFormat;
+}
+
+export interface CellData {
+  value: string;
+  computedValue?: string | number | boolean;
+  formula?: string;
+  style: CellStyle;
+}
+
+export interface CellCoords {
+  col: number;
+  row: number;
+}
+
+export interface Document {
+  id: string;
+  title: string;
+  createdAt: string;
+  updatedAt: string;
+  ownerId: string;
+  rowCount: number;
+  colCount: number;
+
+  preview: Record<string, CellData>;
+}
+
+export interface AuthState {
+  user: { id: string; name: string; email: string } | null;
+  accessToken: string | null;
+  refreshToken: string | null;
+  isAuthenticated: boolean;
+  isLoading: boolean;
+  error: string | null;
+}
